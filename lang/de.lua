195519eda6a9fbd6c53d0c1b2e2c70a0823873fa
Saneroki <GlennD@me.com>
2016-08-24 14:25:24 +0200

    Version 1 final

---
+++ lang/de.lua
@@ -0,0 +1,28 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Glenn
+-- Date: 24/08/2016
+-- Time: 10:56
+-- To change this template use File | Settings | File Templates.
+--
+
+SaneTextEditor = SaneTextEditor or {}
+
+-- TT = Tooltips
+
+
+-- Tooltips
+SaneTextEditor.ColorButtonTT = "Linksklick = Setze farbcode in das textfeld ein. \nRechtsklick = Wähle andere farbe für diesen knopf \nMittelklick = Setze die Farbe zurück auf die standard Farbe \nDie Hilfstooltips können unter den Einstellungen ausgeschalten werden."
+
+
+
+
+
+
+
+
+
+
+
+
+SaneTextEditor.InsertedHelpText = " Hier_Doppelklicken_und_schreiben"
---
+++ lang/en.lua
@@ -0,0 +1,24 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Glenn
+-- Date: 24/08/2016
+-- Time: 10:56
+-- To change this template use File | Settings | File Templates.
+--
+
+SaneTextEditor = SaneTextEditor or {}
+
+-- TT = Tooltips
+
+
+-- Tooltips
+SaneTextEditor.ColorButtonTT = "Leftclick = Insert Colorcode to editbox. \nRightclick = Change color \nMiddleclick = Reset color to default. \nThe Help tooltips can be turned off in the settings."
+SaneTextEditor.ItemButtonTT = "Toggle"
+
+
+
+
+
+
+
+SaneTextEditor.InsertedHelpText = " Doubleclick_here_and_write"
